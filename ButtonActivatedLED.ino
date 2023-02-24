ca0e3589d9c3b59b7da06a1203d9ef6e94849a54
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-02-24 14:29:18 -0500

    Fix the code
    Now it works

--- ButtonActivatedLED.ino
+++ ButtonActivatedLED.ino
@@ -2,18 +2,20 @@ int buttonState = 0;
 
 void setup()
 {
-  pinMode(2, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-  buttonState = digitalRead(2); //read state of the push button
-  if (buttonState == HIGH ){
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
-  else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-  delay (10);
+  buttonState = digitalRead(2); //read state of the push button
+  Serial.println(buttonState);
+  if (buttonState == HIGH ){
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+  delay (10);
 }
