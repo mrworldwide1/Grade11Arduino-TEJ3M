b583a2ec3141ac90e8db547480a60c132b37760c
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-02-28 14:51:48 -0500

    Update Button Timer LED.ino

--- Button Timer LED.ino
+++ Button Timer LED.ino
@@ -1,23 +1,25 @@
 int timer = 0; //keeps track of target time
 int buttonState = digitalRead(2);
+int lightupduration = 1000 //sets length of timer in ms, how long LED is lit up for
 
-void setup()
+void setup() //setup for checking button press status
 {
-  pinMode(2, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, INPUT); //output button push status
+  pinMode(LED_BUILTIN, OUTPUT); //setup LED power pin
 }
 
 void loop()
 {
 buttonState = digitalRead(2);
+Serial.println(buttonState)
 
 if (buttonState == HIGH) {
-   timer = millis() + 1000; //change targest time to 1s from the current time
- }
- if (timer > millis()) {
-   digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-   }
-  delay(10); //delay a little bit to improve performance
+   timer = millis() + lightupduration; //change target time to chosen time from the current time
+ }
+ if (timer > millis()) {
+   digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW); //if time runs out, turn off LED
+   }
+  delay(10); //delay a little bit to improve performance
 }
