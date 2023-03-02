6a6809ec03200249e3051f9596c0624a9786d57a
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-01 21:37:59 -0500

    Update Button Timer LED Assignment.ino

--- Button Timer LED Assignment.ino
+++ Button Timer LED Assignment.ino
@@ -2,6 +2,13 @@
 //TODO: Add potentionemter analog read, and set lightupduration to potentionmeter value via MAT
 //TODO: Add potpin for potentiometer referring to https://docs.arduino.cc/learn/electronics/potentiometer-basics
 
+/*
+Project to make an LED turn on when button is pressed, but also triggering a timer which will determine how long the LED will stay lit. 
+A dial (potentiometer) is used to control the time. Multiple LEDs are utilised as a countdown for the timer.
+By: Lucas Leung for TEJ3M0
+March 1, 2023
+*/
+
 int timer = 0; //keeps track of target time
 int buttonState = 0; //stores button on/off state as var, sets button to off
 int lightUpDuration = 1000; //length of timer in ms, how long LED is lit up for
