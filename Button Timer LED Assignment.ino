191fbc13233a28d1c5b43ecf8248d04acd13a1f5
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-01 21:59:56 -0500

    Update Button Timer LED Assignment.ino

--- Button Timer LED Assignment.ino
+++ Button Timer LED Assignment.ino
@@ -2,17 +2,22 @@
 //TODO: Add potentionemter analog read, and set lightupduration to potentionmeter value via MAT
 //TODO: Add potpin for potentiometer referring to https://docs.arduino.cc/learn/electronics/potentiometer-basics
 
+//TIMER VARIABLES
 int timer = 0; //keeps track of target time
-int buttonState = 0; //initialize var, set button to off
-int lightUpDuration = 1000; //length of timer in ms, how long LED is lit up for
+int lightUpDuration = 1000; //length of timer in ms
+//BUTTON VARIABLES
 int buttonPin = 2; //button output connected to digital pin 2
-int yellowLED = LED_BUILTIN; //yellow LED, connected to builtin LED pin
+int buttonState = 0; //initialize var, set button state to off
 int pressed = 1; //HIGH represented as variable to simplify code. 1 means button is pressed, 0 means no
+//LEDs VARIABLES
+int yellowLED = LED_BUILTIN; //yellow LED, connected to builtin LED pin
+int on = 1; //for the LEDs, HIGH is represented as variable
+int off = 0;//for the LEDs, LOW is represented as variable
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT); //output button push status to button variable
+  pinMode(buttonPin, INPUT); //output button push status as variable
   pinMode(yellowLED, OUTPUT); //setup yellow LED power pin
 }
 
@@ -33,9 +38,9 @@ if (buttonState == pressed) {
    timer = millis() + lightUpDuration; //change target time to chosen time from the current time
  }
  if (timer > millis()) {
-   digitalWrite(yellowLED, HIGH); //turn on LED
+   digitalWrite(yellowLED, on); //turn on LED
   } else {
-    digitalWrite(yellowLED, LOW); //if time runs out, turn off LED
+    digitalWrite(yellowLED, off); //if time runs out, turn off LED
    }
   delay(10); //delay a little bit to improve performance
 }
