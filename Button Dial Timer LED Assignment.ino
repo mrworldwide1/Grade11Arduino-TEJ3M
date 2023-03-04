201d34afa9a542d98ac94049af6e977fa950c279
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-04 16:57:41 -0500

    Update Button Dial Timer LED Assignment.ino

--- Button Dial Timer LED Assignment.ino
+++ Button Dial Timer LED Assignment.ino
@@ -13,7 +13,7 @@ int lightUpDuration = 0; //length of timer in ms
 long totalTime = 0; //keeps track of target time for LED countdown. Using long instead of int, so that the value doesn't overflow. https://www.arduino.cc/reference/en/language/variables/data-types/long/
 int lock = 0; //Pressing the button again once the timer is started doesn't affect it
 long storedMillis = 0; //needed for LED countdown. Using long instead of int to ensure it doesn't overflow. While debugging via serial monitor output, I found that storedMillis turned negative which broke the countdown, only ever ligting up the blue LED.
-//Arduino UNO stores 2 byte value for int, a range of -32,768 to 32,767. That is insufficent for this circuit given that 1000ms is 1 second, so it would only be able to run the countdown up to 32.7 seconds. Whereas long goes up to 2 billion.
+//Arduino UNO stores 2 byte value for int, a range of -32,768 to 32,767. That is insufficent for this circuit given that 1000ms is 1 second, so the circuit could only run for up to 32.7 seconds before it breaks. Whereas long goes up to 2 billion.
 int divideVar = 4; //Required for LED countdown. The number of LEDs present, for this circuit there are 4. 
 
 void setup()
