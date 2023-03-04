c828df8ac708a50ad9b7ae9bbf527487299fdcfc
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-04 12:26:17 -0500

    Update Button Timer LED Assignment.ino

--- Button Timer LED Assignment.ino
+++ Button Timer LED Assignment.ino
@@ -6,19 +6,21 @@
 int timer = 0; //keeps track of target time
 int lightUpDuration = 0; //length of timer in ms
 int buttonPin = 2; //button connected to digital pin 2
-int buttonState = 0; //initialize var, set button state to off
-int pressed = 1; //HIGH represented as variable to simplify code.
+int buttonState = 0; //initialize variable, set button state to off
+int pressed = 1; //HIGH represented as variable to simplify code
 int yellowLED = LED_BUILTIN; //yellow LED, connected to builtin LED pin
-int potPin = A0; //potentiometer should be connected to A0 analog pin
+int redLED = 12; //red LED, connected to pin 12
+int potPin = A0; //potentiometer connected to A0 analog pin
 int potVal = 0;//read value of potentiometer pin
 int lock = 0; //Pressing the button again once the timer is started doesn't affect it
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT); //output button push status as variable
   pinMode(yellowLED, OUTPUT); //setup yellow LED power pin
-  pinMode(potPin, INPUT); //setup potPin variable. sets potentiometer status as variable
+  pinMode(redLED, OUTPUT); //setup red LED power pin
+  pinMode(buttonPin, INPUT); //output button push status as variable
+  pinMode(potPin, INPUT); //setup potentiometer status as variable
 }
 
 void loop()
@@ -46,7 +48,7 @@ if (buttonState == pressed) {
 } else {
 	if (lock == 1) {
 	Serial.print("Button unpressed & locked. Time (ms): ");
-    	Serial.println(lightUpDuration);
+    Serial.println(lightUpDuration);
 	//Serial.println(timer);
 	//Serial.println(millis());
     } else {
@@ -57,14 +59,17 @@ if (buttonState == pressed) {
     }
 }
 
+//TURNS ON AND TURNS OFF LEDs
 if (buttonState == pressed && lock == 0) {
   timer = millis() + lightUpDuration; //change target time to chosen time from the current time
   lock = 1;
  }
  if (timer > millis()) {
-   digitalWrite(yellowLED, HIGH); //turn on LED
+   digitalWrite(yellowLED, HIGH); //turn on yellow LED
+   digitalWrite(redLED, HIGH); //turn on red LED
   } else {
-   digitalWrite(yellowLED, LOW); //if time runs out, turn off LED
+   digitalWrite(yellowLED, LOW); //if time runs out, turn off yellow LED
+   digitalWrite(redLED, LOW); //turn on red LED
    lock = 0;
    }
   delay(10); //delay a little bit to improve performance
