cba7d23819dc43b5ff76b5ea2bf22170b78ac68b
Lucas L <66652566+mrworldwide1@users.noreply.github.com>
2023-03-05 12:32:23 -0500

    Update Button Dial Timer LED Assignment.ino

--- Button Dial Timer LED Assignment.ino
+++ Button Dial Timer LED Assignment.ino
@@ -1,7 +1,7 @@
 //TASK: Create a circuit where when a button is pressed an LED will turn on. Also triggers a timer which will determine how long the LED will stay lit. A dial (potentiometer) used to control the time.
 
 int buttonPin = 2; //button connected to digital pin 2
-int buttonState = 0; //initialize variable, set button state to off
+int buttonState = 0; //define variable. set button state to off
 int pressed = 1; //HIGH represented as variable to simplify code
 int lock = 0; //Pressing the button again once the timer is started doesn't affect it
 int blueLED = 10; //blue LED, connected to pin 10
@@ -10,11 +10,11 @@ int greenLED = 11; //green LED, connected to pin 11
 int redLED = LED_BUILTIN; //red LED, connected to builtin LED pin
 int potPin = A0; //potentiometer connected to A0 analog pin
 int potVal = 0;//read value of potentiometer pin
-int lightUpDuration = 0; //length of timer in ms, selected by dial
-long totalTime = 0; //keeps track of target time for LED countdown. Using long instead of int, so that the value doesn't overflow.
+int lightUpDuration = 0; //define variable. length of timer in ms, selected by dial
+long totalTime = 0; //define variable. keeps track of target time for LED countdown. Using long instead of int so the value doesn't overflow.
 long storedMillis = 0; //needed for LED countdown. Using long instead of int to ensure it doesn't overflow. While debugging via serial monitor output, I found that storedMillis turned negative which broke the countdown.
-//Arduino UNO stores 2 byte value for int, a range of -32,768 to 32,767. That's insufficent for this circuit given that 1000ms is 1 second, so the circuit could only run for up to 32.7 seconds before it breaks. Whereas long goes up to 2 billion.
-int divideVar = 0; //initilaize variable, required for LED countdown
+//Arduino UNO stores a range of -32,768 to 32,767 for integers. That's insufficent for this circuit given that 1000ms is 1 second, so the circuit could only run for up to 32.7 seconds before it breaks. Whereas long goes up to 2 billion.
+int divideVar = 0; //define variable, required for LED countdown
 
 void setup()
 {
@@ -31,10 +31,10 @@ void loop()
 {
 buttonState = digitalRead(buttonPin); //store button status as variable
 
-//potentiometer
+//potentiometer setup
 potVal = analogRead(potPin);
 int lightUpDuration = potVal;
-lightUpDuration = map(lightUpDuration, 0, 1023, 1000, 10000); //The dial can set the time from 1 second to 10 seconds. 1000ms(1s)-10000ms(10s)
+lightUpDuration = map(lightUpDuration, 0, 1023, 1000, 10000); //dial can set the time from 1 second to 10 seconds. 1000ms(1s)-10000ms(10s)
   
 //print status of button and potentiometer for debugging. To debug millis, totalTime or storedMillis, uncomment the respective lines
 if (buttonState == pressed) {
@@ -68,7 +68,7 @@ if (buttonState == pressed) {
 }
 
 //TURN ON AND TURN OFF LEDs, DISPLAYING COUNTDOWN EFFECT
-//EXPLANATION: to get the countdown effect working for the 4 LEDs each LED turns on at 4 intervals. specifically, when 100% of the timer remains, when 75% remains, 50% remains, 25%, and lastly 0%.
+//EXPLANATION: to get countdown effect working for the 4 LEDs each LED turns on at 4 intervals. specifically, when 100% of the timer remains, when 75% remains, 50% remains, 25%, and lastly 0%.
 //Example: 2000ms millis & dial set to 5000ms. all LEDs will turn off once millis reaches 7000ms. there are 4 LEDs so divide dial time by 4 (5000/4 = 1000). immediately after pressing button, blue LED is lit.
 //then, when millis reaches 4000ms, green LED is lit. At 5000ms, yellow LED is lit. 6000ms, red LED is lit. at 7000ms, all LEDs are turned off.
 //2000ms + 1000ms = 3000ms, 2000ms + (1000ms + 1000ms) = 4000ms, 2000ms + (1000ms + 1000ms + 1000ms) = 5000ms, etc.
@@ -77,7 +77,7 @@ if (buttonState == pressed && lock == 0) {
  lock = 1; //pressing the button won't do anything until countdown is over
  storedMillis = millis(); //notes millis when the button was pressed
  divideVar = lightUpDuration / 4; //4 LEDs used, 4 LED lightup intervals needed
- totalTime = storedMillis + lightUpDuration; //change target time to chosen time
+ totalTime = storedMillis + lightUpDuration; //determine the time at which all LEDs will turn off
  }
 
  if (millis() <= (storedMillis + divideVar)) {
